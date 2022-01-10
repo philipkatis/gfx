9c6fa6fdfa7b6fb35044f3c3ad79c0ffc59158f0
Philip Katis <packkatis@gmail.com>
2022-01-11 01:07:00 +0200

    Loaded the required WGL extensions.

--- code/gfx_win32.c
+++ code/gfx_win32.c
@@ -3,6 +3,18 @@
 
 #include "gfx_base.h"
 
+// TODO(philip): Move these to a header file.
+// TODO(philip): Documentation.
+
+typedef BOOL wgl_choose_pixel_format_arb(HDC hdc, const int *piAttribIList, const FLOAT *pfAttribFList,
+                                         UINT nMaxFormats, int *piFormats, UINT *nNumFormats);
+typedef HGLRC wgl_create_context_attribs_arb(HDC hDC, HGLRC hShareContext, const int *attribList);
+typedef BOOL wgl_swap_interval_ext(int interval);
+
+static wgl_choose_pixel_format_arb *wglChoosePixelFormatARB = 0;
+static wgl_create_context_attribs_arb *wglCreateContextAttribsARB = 0;
+static wgl_swap_interval_ext *wglSwapIntervalEXT = 0;
+
 static LRESULT
 Win32WindowProcedure(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam)
 {
@@ -31,9 +43,93 @@ Win32WindowProcedure(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam)
     return Result;
 }
 
+// TODO(philip): Documentation.
+static void
+Win32LoadWLGExtensions(HINSTANCE Instance)
+{
+    LPCSTR WindowClassName = "gfx_dummy_win32_window_class";
+
+    WNDCLASSA WindowClass = {0};
+    WindowClass.style = CS_OWNDC;
+    WindowClass.lpfnWndProc = DefWindowProcA;
+    WindowClass.hInstance = Instance;
+    WindowClass.lpszClassName = WindowClassName;
+
+    if (RegisterClassA(&WindowClass))
+    {
+        HWND Window = CreateWindowA(WindowClassName, 0, 0, 0, 0, 0, 0, 0, 0, Instance, 0);
+        if (Window)
+        {
+            HDC DeviceContext = GetDC(Window);
+
+            PIXELFORMATDESCRIPTOR DesiredPixelFormat = {0};
+            DesiredPixelFormat.nSize = sizeof(PIXELFORMATDESCRIPTOR);
+            DesiredPixelFormat.nVersion = 1;
+            DesiredPixelFormat.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+            DesiredPixelFormat.iPixelType = PFD_TYPE_RGBA;
+            DesiredPixelFormat.cColorBits = 32;
+            DesiredPixelFormat.cDepthBits = 24;
+            DesiredPixelFormat.cStencilBits = 8;
+
+            s32 SelectedPixelFormatIndex = ChoosePixelFormat(DeviceContext, &DesiredPixelFormat);
+            if (SelectedPixelFormatIndex)
+            {
+                PIXELFORMATDESCRIPTOR SelectedPixelFormat;
+                DescribePixelFormat(DeviceContext, SelectedPixelFormatIndex, sizeof(PIXELFORMATDESCRIPTOR),
+                                    &SelectedPixelFormat);
+
+                if (SetPixelFormat(DeviceContext, SelectedPixelFormatIndex, &SelectedPixelFormat))
+                {
+                    HGLRC OpenGLContext = wglCreateContext(DeviceContext);
+                    if (OpenGLContext)
+                    {
+                        wglMakeCurrent(DeviceContext, OpenGLContext);
+
+                        wglChoosePixelFormatARB = (wgl_choose_pixel_format_arb *)wglGetProcAddress("wglChoosePixelFormatARB");
+                        wglCreateContextAttribsARB = (wgl_create_context_attribs_arb *)wglGetProcAddress("wglCreateContextAttribsARB");
+                        wglSwapIntervalEXT = (wgl_swap_interval_ext *)wglGetProcAddress("wglSwapIntervalEXT");
+
+                        // TODO(philip): Investigate what we should do if loading one of these fails.
+                    }
+                    else
+                    {
+                        // TODO(philip): Error message.
+                    }
+
+                    wglDeleteContext(OpenGLContext);
+                }
+                else
+                {
+                    // TODO(philip): Error message.
+                }
+            }
+            else
+            {
+                // TODO(philip): Error message.
+            }
+
+            ReleaseDC(Window, DeviceContext);
+        }
+        else
+        {
+            // TODO(philip): Error message.
+        }
+
+        DestroyWindow(Window);
+    }
+    else
+    {
+        // TODO(philip): Error message.
+    }
+
+    UnregisterClassA(WindowClassName, Instance);
+}
+
 s32
 WinMain(HINSTANCE Instance, HINSTANCE PreviousInstance, LPSTR Arguments, s32 ShowCMD)
 {
+    Win32LoadWLGExtensions(Instance);
+
     LPCSTR WindowClassName = "gfx_win32_window_class";
 
     WNDCLASSA WindowClass = {0};
