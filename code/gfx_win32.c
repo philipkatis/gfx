c23b0e95876bf2e22529ebd754f9c82815fc4c87
Philip Katis <packkatis@gmail.com>
2022-01-11 00:28:36 +0200

    Opened a window using the Win32 API.

--- code/gfx_win32.c
+++ code/gfx_win32.c
@@ -1,8 +1,86 @@
-// TODO(philip): Switch to using WinMain.
+// TODO(philip): Remove junk from the Windows header file.
+#include <Windows.h>
+
 // TODO(philip): Define base types.
+// TODO(philip): Define base keywords.
+
+static LRESULT
+Win32WindowProcedure(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam)
+{
+    LRESULT Result = 0;
+
+    switch (Message)
+    {
+        // TODO(philip): Close the application when we press escape.
+
+        case WM_CLOSE:
+        {
+            DestroyWindow(Window);
+        } break;
+
+        case WM_DESTROY:
+        {
+            PostQuitMessage(0);
+        } break;
+
+        default:
+        {
+            Result = DefWindowProcA(Window, Message, WParam, LParam);
+        } break;
+    }
+
+    return Result;
+}
 
 int
-main(int ArgumentCount, char **Arguments)
+WinMain(HINSTANCE Instance, HINSTANCE PreviousInstance, LPSTR Arguments, int ShowCMD)
 {
+    LPCSTR WindowClassName = "gfx_win32_window_class";
+
+    WNDCLASSA WindowClass = {0};
+    WindowClass.style = CS_VREDRAW | CS_HREDRAW | CS_OWNDC;
+    WindowClass.lpfnWndProc = Win32WindowProcedure;
+    WindowClass.hInstance = Instance;
+    WindowClass.lpszClassName = WindowClassName;
+
+    if (RegisterClassA(&WindowClass))
+    {
+        // TODO(philip): Create the window based on the client area dimensions.
+        // TODO(philip): Center the window on the screen.
+        HWND Window = CreateWindowA(WindowClassName, "gfx", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
+                                    CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, Instance, 0);
+
+        if (Window)
+        {
+            ShowWindow(Window, SW_SHOW);
+
+            for (;;)
+            {
+                MSG Message;
+                while (PeekMessageA(&Message, 0, 0, 0, PM_REMOVE))
+                {
+                    if (Message.message == WM_QUIT)
+                    {
+                        // TODO(philip): Replace with a boolean.
+                        goto Exit;
+                    }
+
+                    TranslateMessage(&Message);
+                    DispatchMessageA(&Message);
+                }
+            }
+        }
+        else
+        {
+            // TODO(philip): Error message.
+        }
+    }
+    else
+    {
+        // TODO(philip): Error message.
+    }
+
+    Exit:
+
     return 0;
-}
+};
